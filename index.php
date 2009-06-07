ddf18b9fcc957c845811172a2922d7a919516cd0
Christian Carey <ccarey@cccubu.(none)>
2009-06-06 20:38:34 -0700

    add first iteration of the Dispatcher class

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
+<?php
+include 'lib/dispatcher.php';
+$basedir = "/dispatcher/";
+$dispatcher = new Dispatcher();
+?>
 <html>
     <head>
         <title>PHP Url Dispatcher</title>
     </head>
     <body>
         <h1>PHP Url Dispatcher</h1>
+        <ul>
+            <li><b>Controller:</b> <?php echo $dispatcher->controller; ?></li>
+            <li><b>Action:</b> <?php echo $dispatcher->action; ?></li>
+            <li>
+                <b>Parameters:</b>
+                <ul>
+                <?php
+                foreach ($dispatcher->parameters as $param) {
+                    echo '<li>'.$param.'</li>';
+                }
+                ?>
+                </ul>
+            </li>
+        </ul>
+        <h3>Test Links</h3>
+        <ul>
+            <li><a href="<?php echo $basedir; ?>test/">Test - no action</a></li>
+            <li><a href="<?php echo $basedir; ?>test/show">Test - show</a></li>
+            <li><a href="<?php echo $basedir; ?>test/show/param1/param2">Test - show - with parameters"</a></li>
+            <li><a href="<?php echo $basedir; ?>">Home</a></li>
+        </ul>
     </body>
 </html>
---
+++ lib/dispatcher.php
@@ -0,0 +1,28 @@
+<?php
+class Dispatcher {
+    var $controller;
+    var $action;
+    var $parameters;
+    
+    function Dispatcher() {
+        $requestURI = explode('/', $_SERVER['REQUEST_URI']);
+        $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
+        
+        $commandArray = array_diff_assoc($requestURI, $scriptName);
+        $commandArray = array_values($commandArray);
+        
+        $this->controller = $commandArray[0];
+        $this->action = $commandArray[1];
+        $this->parameters = array_slice($commandArray, 2);
+
+        // Check if the url is the root.
+        // if it is then set the command to the root controller.
+        // and _default function.
+        if ($this->controller == '') {
+            $this->controller = 'root';
+        }
+
+        //$this->Command = new Axial_Command($controllerName, $controllerFunction, $parameters);
+    }
+}
+?>
