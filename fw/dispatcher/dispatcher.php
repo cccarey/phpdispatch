06535b016d7377c4d05d03e7b3f28f97baefbb9c
Christian Carey <ccarey@apophxf130dh1.(none)>
2009-10-10 18:57:14 -0700

    update the dispatcher so it properly reports controller existence, but defaults to the root controller.

--- fw/dispatcher/dispatcher.php
+++ fw/dispatcher/dispatcher.php
@@ -1,5 +1,6 @@
 <?php
 class Dispatcher {
+    var $reqController;
     var $controller;
     var $controllerScript;
     var $controllerClass;
@@ -13,28 +14,42 @@ class Dispatcher {
         $commandArray = array_diff_assoc($requestURI, $scriptName);
         $commandArray = array_values($commandArray);
         
-        $this->controller = $commandArray[0];
+        $this->reqController = $commandArray[0];
+        
+        // Check if the url is the root.
+        // if it is then set the command to the root controller
+        // and _default function.
+        if ($this->reqController == '') {
+            // root controller is no the root of the folder, i.e. no requested controller
+            $this->setController();
+        }
+        else {
+            $this->setController($this->reqController);
+        }
         $this->action = $commandArray[1];
         $this->parameters = array_slice($commandArray, 2);
-
-        // Check if the url is the root.
-        // if it is then set the command to the root controller.
-        // and _default function.
-        if ($this->controller == '') {
-            $this->controller = 'root';
+    }
+    
+    function setController($name='root') {
+        if ($name == '') {
+            // if $name is set and empty, set to the root controller
+            $name = 'root';
         }
-
+        $this->controller = $name;
         $this->controllerScript = 'app/controllers/'.$this->controller.'.php';
         $this->controllerClass = $this->controller.'Controller';
     }
     
     function controllerExists() {
-        return file_exists($this->controllerScript);
+        if ($this->controller == $this->reqController || $this->reqController == '') {
+            return file_exists($this->controllerScript);
+        }
+        return false;
     }
     
     function dispatch() {
         if (!$this->controllerExists()) {
-            return;
+            $this->setController();
         }
         include $this->controllerScript;
         $controller = new $this->controllerClass();
